8a213c9a0ba7d63cba123e8377263c22f2984984
Sam Rotthier <sam.rotthier@gmail.com>
2024-09-20 18:40:54 +0200

    Passwordchecker

---
+++ Util/PasswordChecker/PasswordChecker.py
@@ -0,0 +1,20 @@
+
+txt_path = 'Python_SmallProjects/Util/PasswordChecker/passwords.txt'
+
+def check_password(password: str):
+    with open(txt_path, 'r') as file:
+        common_passwords: list[str] = file.read().splitlines()
+        #print(common_passwords)
+    
+    for i, common_password in enumerate(common_passwords, start=1):
+        if password.lower() == common_password: #the .lower it to check even if you have capital letters
+            print(f'{password}: ❌ (#{i})')
+            return
+    print(f'{password}:✅ (Unique)')
+
+def main():
+    user_password: str = input('Enter a password: ')
+    check_password(user_password)
+
+if __name__ == '__main__':
+    main()
